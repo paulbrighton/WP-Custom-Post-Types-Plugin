77ec401cbfd391d0fab45c0253c0a4ddac0d5509
Paul Brighton <brighton.paul@yahoo.com>
2020-06-19 10:45:30 +0100

    Added the portfolio post type and registered the skills and project type taxonomies

--- index.php
+++ index.php
@@ -21,3 +21,20 @@ require_once 'includes/portfolio-post-type.php';
 require_once 'includes/project-type-tax.php';
 require_once 'includes/skills-tax.php';
 
+function _themename__pluginname_activate() {
+  _themename__pluginname_setup_post_type();
+  _themename__pluginname_register_skills_tax();
+  _themename__pluginname_register_project_type_tax();
+  flush_rewrite_rules();
+}
+
+register_activation_hook(__FILE__, '_themename__pluginname_activate');
+
+function _themename__pluginname_deactivate() {
+  unregister_post_type('_themename_portfolio');
+  unregister_taxonomy('_themename_project_type');
+  unregister_taxonomy('_themename_skills');
+  flush_rewrite_rules();
+}
+
+register_deactivation_hook(__FILE__, '_themename__pluginname_deactivate');
